65bf5f2f3dc70d5cae76253b7e680489465ac130
ogourment <ogourment@smarterportal.com>
2015-02-09 23:30:28 +0100

    Internationalize Accounts UI texts

--- client/accounts/accounts.js
+++ client/accounts/accounts.js
@@ -3,49 +3,53 @@
   AccountsTemplates.configureRoute('signUp');
   AccountsTemplates.configureRoute('signIn');
 
-  AccountsTemplates.configure({
-    // Behaviour
-    confirmPassword: true,
-    enablePasswordChange: true,
-    forbidClientAccountCreation: false,
-    overrideLoginErrors: true,
-    sendVerificationEmail: false,
+  // Configure *after* TAPi18n is initialized..
+  Meteor.startup( function () {
+    
+    AccountsTemplates.configure({
+      // Behaviour
+      confirmPassword: true,
+      enablePasswordChange: true,
+      forbidClientAccountCreation: false,
+      overrideLoginErrors: true,
+      sendVerificationEmail: false,
 
-    // Appearance
-    showAddRemoveServices: false,
-    showForgotPasswordLink: true,
-    showLabels: true,
-    showPlaceholders: true,
+      // Appearance
+      showAddRemoveServices: false,
+      showForgotPasswordLink: true,
+      showLabels: true,
+      showPlaceholders: true,
 
-    // Client-side Validation
-    continuousValidation: false,
-    negativeFeedback: false,
-    negativeValidation: true,
-    positiveValidation: true,
-    positiveFeedback: true,
-    showValidating: true,
+      // Client-side Validation
+      continuousValidation: false,
+      negativeFeedback: false,
+      negativeValidation: true,
+      positiveValidation: true,
+      positiveFeedback: true,
+      showValidating: true,
 
-    // Privacy Policy and Terms of Use
-    //privacyUrl: 'privacy',
-    //termsUrl: 'terms-of-use',
+      // Privacy Policy and Terms of Use
+      //privacyUrl: 'privacy',
+      //termsUrl: 'terms-of-use',
 
-    // Redirects
-    homeRoutePath: '/',
-    redirectTimeout: 4000,
+      // Redirects
+      homeRoutePath: '/',
+      redirectTimeout: 4000,
 
-    // Texts
-    texts: {
-      button: {
-        signUp: "Register Now!"
-      },
-      socialSignUp: "Register",
-      socialIcons: {
-        "meteor-developer": "fa fa-rocket"
-      },
-      title: {
-        forgotPwd: "Recover Your Password"
+      // Texts
+      texts: {
+        button: {
+          signUp: TAPi18n.__('register_now')
+        },
+        socialSignUp: TAPi18n.__('register'),
+        socialIcons: {
+          "meteor-developer": "fa fa-rocket"
+        },
+        title: {
+          forgotPwd: TAPi18n.__('recover_password')
+        }
       }
-    }
+    });
   });
 
 })();

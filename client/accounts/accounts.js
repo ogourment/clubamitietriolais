7f72dcdf5e92b1e7f08c7e462ae018a0cb9172e9
ogourment <ogourment@smarterportal.com>
2015-02-18 00:02:54 +0100

    Authors can now create author accounts

--- client/accounts/accounts.js
+++ client/accounts/accounts.js
@@ -12,17 +12,18 @@ function getCredentials (template) {
 function accountsError(template, key) {
 
   console.debug(key, TAPi18n.__(key));
-  template.find('#accounts-error').innerHTML = '<p>' + TAPi18n.__(key) + '</p>';
+  template.find('#accounts-message').innerHTML =
+    '<p class="blink">' + TAPi18n.__(key) + '</p>';
 };
 
 Template.login.events({
 
   'submit .accounts-form': function(event, template) {
 
-    console.log("Login...");
-
     event.preventDefault();
     var cred = getCredentials(template);
+
+    console.log("Signing in:", cred.email);
 
     Meteor.loginWithPassword(cred.email, cred.password, function (err) {
 
@@ -42,34 +43,40 @@ Template.login.events({
 
 Template.register.events({
 
-  'submit .accounts-form': function(event, template) {
-
-    console.log("Registering...");
+  'submit .accounts-form': function (event, template) {
 
     event.preventDefault();
     var cred = getCredentials(template);
     var password2 = template.find('#accounts-password2').value;
     if (cred.password === password2) {
 
-      Accounts.createUser(cred, function (err) {
+      console.log("Creator:", Meteor.user().emails[0].address, " creates user:", cred.email);
 
-        if (err) {
-          if (err.reason.toString() === 'Email already exists.') {
-            console.error('Email already exists:', err);
+      cred.isAuthor = template.find('#author').checked;
+
+      Meteor.apply("createBlogUser", [ cred ], function (error, result) {
+
+        if (error) {
+          console.error(error);
+          if (error.reason.toString() === 'Email already exists.') {
+            console.error('Email already exists:', error);
             accountsError(template, 'register_email_exists');
           } else {
-            console.error(err);
+            console.error(error);
             accountsError(template, 'register_error');
           }
+          return false;
         }
-        else {
-          console.info("Registration successful for:", cred.email);
-          Router.go('/blog');
-        }
+
+        console.info("Registration successful for:", cred.email);
+        _.each(['email', 'password', 'password2'], function (field) {
+          template.find("#accounts-" + field).value = '';
+        });
+        template.find('#accounts-message').innerHTML =
+          '<h3>' + TAPi18n.__('account_created_success', cred.email) + '</h3>';
       });
     }
     else {
-      console.error('Passwords don\'t match');
       accountsError(template, 'passwords_dont_match');
     }
     return false;
